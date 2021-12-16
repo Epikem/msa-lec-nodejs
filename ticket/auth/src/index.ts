482664910a4ed7a1cfb2200da5576fd35b901e0c
epikem <epikem@kakao.com>
2021-12-16 18:30:38 +0900

    split auth currentUser route - 130

--- ticket/auth/src/index.ts
+++ ticket/auth/src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
+import { CurrentUserRouter } from './routes/current-user';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('hi there!');
-});
+app.use(CurrentUserRouter);
 
 app.listen(3000, () => {
     console.log('auth service listening on 3000');
