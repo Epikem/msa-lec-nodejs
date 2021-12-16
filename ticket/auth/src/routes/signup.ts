06b515a7b37346f3a35849074632951b6bad359a
epikem <epikem@kakao.com>
2021-12-16 19:34:17 +0900

    use validation on signup

--- ticket/auth/src/routes/signup.ts
+++ ticket/auth/src/routes/signup.ts
@@ -1,9 +1,34 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('hi there!!!');
-})
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({
+        min: 4,
+        max: 20,
+      })
+      .withMessage('Password must be between 4 and 20 chars'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    // new User({email, password})
+    console.log('creating a user...!');
+
+    res.send({ email, password });
+  }
+);
 
 export { router as signupRouter };
