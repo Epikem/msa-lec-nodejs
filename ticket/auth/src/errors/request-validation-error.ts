32ca1d535cd64525bb5faefd6b0ab8e37d26cb64
epikem <epikem@kakao.com>
2021-12-17 21:02:19 +0900

    add custom error abstract class - 146

--- ticket/auth/src/errors/request-validation-error.ts
+++ ticket/auth/src/errors/request-validation-error.ts
@@ -1,22 +1,19 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super('invalid request parameters');
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
 
   serializeErrors() {
-    return [
-      {
-        message: this.errors.map((err) => ({
-          message: err.msg,
-          field: err.param,
-        })),
-      },
-    ];
+    return this.errors.map((err) => ({
+      message: err.msg,
+      field: err.param,
+    }));
   }
 }
