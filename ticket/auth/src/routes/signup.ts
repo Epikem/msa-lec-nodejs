d63dae9c795754558c1af76bfe1d16831496eebc
epikem <epikem@kakao.com>
2021-12-17 19:32:20 +0900

    add custom errors

--- ticket/auth/src/routes/signup.ts
+++ ticket/auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
@@ -19,13 +21,15 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      throw new Error('Invalid email or password');
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
     // new User({email, password})
     console.log('creating a user...!');
+
+    throw new DatabaseConnectionError();
 
     res.send({ email, password });
   }
